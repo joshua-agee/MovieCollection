bb30ca6608dee48928731f785db8f70117399905
Joshua Agee <joshua.agee@protonmail.com>
2020-07-08 10:28:46 -0500

    movie and user schemas

--- models/movie.js
+++ models/movie.js
@@ -2,3 +2,35 @@
 //details from movie
 //comments from user
 //rating
+
+
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    id: {type: String}, //the imdb movie ex: "tt1375666"
+    title: {type: String}, // short title: Inception
+    fullTitle: {type: String}, // full title "Inception (2010)"
+    type: {type: String}, //"Movie", "TV"
+    year: {type: String}, 
+    image: {type: String}, //link to poster
+    runtimeStr: {type: String}, //"2h 28 mins"
+    plot: {type: String}, // longer text of plot
+    directorList: [{
+        id: {type: String}, //"nm0634240" person id
+        name: {type: String}, //"Christopher Nolan" 
+    }],
+    starList: [{
+        id: {type: String}, //person id
+        name: {type: String} //name
+    }],
+    actorList: [{
+        id: {type: String}, //person id
+        image: {type: String}, //headshot
+        name: {type: String}, //name
+        asCharacter: {type: String}, // name of char in role
+    }],
+})
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
--- models/user.js
+++ models/user.js
@@ -1,2 +1,19 @@
 //model for a user
 //username collection, password
+
+const mongoose = require('mongoose');
+const Movie = require('./movie.js');
+
+const userSchema = new mongoose.Schema({
+    username:  {type: String, required: true}, //login name
+    passwordHash: {type: String, required: true}, //password hash
+    nickname: {type: String}, // friendly name
+    movieCollection: [{
+        movie: Movie.schema,
+        userComments: {type: String},
+    }],
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
